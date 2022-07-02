969d6d04e9cb7e1c894fc620f1819ae24b68c512
DenysSergeev <sergeevdenys@gmail.com>
2022-07-02 14:44:26 +0300

    3rd task in process...

--- script.js
+++ script.js
@@ -82,3 +82,24 @@ window.addEventListener("load", () => {
     PHONE_NUMBER_NODE.removeEventListener("click", handlerNumberClick);
   }
 });
+
+
+/*
+3) Доработка кода (advanced level):
+Данный скрипт исполняется в GTM и внедряется в google analytics методом custom Task.
+Скрипт высылает данные о поведении пользователя на сервер Optimozg и затем в Bigquery(файл bq.php обрабатывает и пересылает данные). Данные на сервер Optimozg приходят исправно, но данные в Google Analytics не доходят. 
+В чем причина? 
+Как это исправить?
+Подсказка:
+(протестируйте код на своем экземпляре сайта с GTM)
+
+
+function(){return function(tracker){if("undefined"===typeof tracker.get("BigQueryStreaming")){var f=tracker.get("sendHitTask"),h=function(){function d(c){var a=!1;try{document.createElement("img").src=e(!0)+"?"+c,a=!0}catch(k){}
+return a}
+function e(c){var a="https://test.optimozg.com/bq/bq-test.php";c||(a+="?tid="+encodeURIComponent(tracker.get("trackingId")));
+return a}
+return{send:function(c){var a;if(!(a=2036>=c.length&&d(c))){a=!1;try{a=navigator.sendBeacon&&navigator.sendBeacon(e(),c)}catch(g){}}
+if(!a){a=!1;var b;try{window.XMLHttpRequest&&"withCredentials" in(b=new XMLHttpRequest)&&(b.open("POST",e(),!0),b.setRequestHeader("Content-Type","text/plain"),b.send(c),a=!0)}catch(g){}}
+return a||d(c)}}}();tracker.set("sendHitTask",function(d){h.send(d.get("hitPayload"));tracker.set("BigQueryStreaming",!0)})}}}
+
+*/
